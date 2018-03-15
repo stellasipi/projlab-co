ae465211576ee2066b37738c7051429162c7f06f
Unknown <dianajaborszki@gmail.com>
2018-03-15 17:38:29 +0100

    Split-re képes menü

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package shokoban;
+﻿package shokoban;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,30 +7,30 @@ import java.io.InputStreamReader;
 public class Main {
 
 	public static void main(String[] args) {
-		int szam; //beolvasott sorszáma
+		int[] szam=new int[2]; //beolvasott sorszáma
 		System.out.println("Menu:\n"+ 
-				"1. Pálya felépítése\n" + 
-				"2. Munkás eltol egy ládát egy üres mezőre\n" + 
-				"	2.1. A mező, amire toljuk fal legyen? (Y/N)\n" + 
-				"	2.2. A mező, amire toljuk oszlop legyen? (Y/N)\n" + 
-				"	2.3. A mező, amire toljuk csapda legyen? (Y/N)\n" + 
-				"	2.4. A mező, amire toljuk lyuk legyen? (Y/N)\n" + 
-				"	2.5. A mező, amire toljuk cél legyen? (Y/N)\n" + 
-				"	2.6. A mező, amire toljuk kapcsoló legyen? (Y/N)\n" + 
-				"	2.7. A mező, amire toljuk sima mező legyen? (Y/N)\n" + 
-				"3. Munkás eltol egy ládát egy adott mezőről (és egy sima üres mezőre érkezik)\n" + 
-				"	3.1. A mező, amiről toljuk csapda legyen? (Y/N)\n" + 
-				"	3.2. A mező, amiről toljuk cél legyen? (Y/N)\n" + 
-				"	3.3. A mező, amiről toljuk kapcsoló legyen? (Y/N)\n" + 
-				"	3.4. A mező, amiről toljuk sima mező legyen? (Y/N)\n" + 
-				"4. Munkás lyukba lép\n" + 
-				"5. Láda ládát tol\n" + 
-				"6. Munkás munkást tol\n" + 
-				"7. MLMF\n" + 
-				"8. MLMMF" +
+				"1.0 Pálya felépítése\n" + 
+				"2.0 Munkás eltol egy ládát egy üres mezőre\n" + 
+				"	2.1 A mező, amire toljuk fal legyen? (Y/N)\n" + 
+				"	2.2 A mező, amire toljuk oszlop legyen? (Y/N)\n" + 
+				"	2.3 A mező, amire toljuk csapda legyen? (Y/N)\n" + 
+				"	2.4 A mező, amire toljuk lyuk legyen? (Y/N)\n" + 
+				"	2.5 A mező, amire toljuk cél legyen? (Y/N)\n" + 
+				"	2.6 A mező, amire toljuk kapcsoló legyen? (Y/N)\n" + 
+				"	2.7 A mező, amire toljuk sima mező legyen? (Y/N)\n" + 
+				"3.0 Munkás eltol egy ládát egy adott mezőről (és egy sima üres mezőre érkezik)\n" + 
+				"	3.1 A mező, amiről toljuk csapda legyen? (Y/N)\n" + 
+				"	3.2 A mező, amiről toljuk cél legyen? (Y/N)\n" + 
+				"	3.3 A mező, amiről toljuk kapcsoló legyen? (Y/N)\n" + 
+				"	3.4 A mező, amiről toljuk sima mező legyen? (Y/N)\n" + 
+				"4 Munkás lyukba lép\n" + 
+				"5 Láda ládát tol\n" + 
+				"6 Munkás munkást tol\n" + 
+				"7 MLMF\n" + 
+				"8 MLMMF\n" +
 				"Használati útmutató: ...");
     	String input = null;
-		while(input!="e"){
+		while(true){
 			System.out.println("? Válassz egy dialógust: ");
 		
 			//beolvasás	
@@ -38,13 +38,29 @@ public class Main {
 	    	try {
 	        	BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 	        	input = bufferedReader.readLine();
-	        	szam = Integer.parseInt(input);
-	        	System.out.println(szam);
+	        	if(input.equals("e"))
+	        	{
+	        		break;
+	        	}
+	        	else
+	        	{
+	        		
+		        	String split[]=input.split("\\.");
+		        	for(int i=0;i < split.length;i++)
+		        	{
+		        		szam[i] = Integer.parseInt(split[i]);
+		        	}
+		        	System.out.println(szam[0]+"."+szam[1]);
+	        	}
+
+	        	
 	    	} catch (NumberFormatException ex) {
 	    		System.out.println("Not a number !");
 	    	} catch (IOException e) {
 	        	e.printStackTrace();
 	    	}
+			
+			
 		}
 	}
 
