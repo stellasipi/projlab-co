ce3b15e527c2133dda505af2a49d524091901868
Unknown <stellasipi@gmail.com>
2018-03-17 13:18:48 +0100

    Button, Crate - függvények félig kész

--- Button.java
+++ Button.java
@@ -1,5 +1,26 @@
 package shokoban;
 
 public class Button extends TileElement {
-	//Marci
+	private Trap trap;
+	
+	public void Accept(Crate c) {
+		
+	}
+	
+	public void Remove(Crate c) {
+		
+	}
+	
+	public void Switch(Trap t) {
+		
+	}
+
+	public Trap getTrap() {
+		return trap;
+	}
+
+	public void setTrap(Trap trap) {
+		this.trap = trap;
+	}
+	
 }
--- Crate.java
+++ Crate.java
@@ -1,5 +1,54 @@
 package shokoban;
 
 public class Crate extends Object {
-	//eukjdgdjfhr
+	private int score;
+	private Worker pushedBy;
+	
+	public void visit(Coloumn c ,Direction d) {
+		
+	}
+	
+	public void visit(Target target ,Direction d) {
+		
+	}
+	public void visit(Hole h ,Direction d) {
+		
+	}
+	
+	public void visit(Trap trap ,Direction d) {
+		
+	}
+	
+	public void visit(Button b ,Direction d) {
+		
+	}
+	
+	public void visit(Tile tile ,Direction d) {
+		
+	}
+	
+	public void visit(Wall w ,Direction d) {
+		
+	}
+	
+	public void push(Object o, Direction d) {
+		
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public Worker getPushedBy() {
+		return pushedBy;
+	}
+
+	public void setPushedBy(Worker pushedBy) {
+		this.pushedBy = pushedBy;
+	}
+
 }
