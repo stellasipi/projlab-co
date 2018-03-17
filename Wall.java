0e6aa2f4892fcb3bca18ab8ff1b5ee07ac1c7534
Unknown <radly96@gmail.com>
2018-03-17 21:21:52 +0100

    TileElement, Coloumn, Wall, Direction, Object

--- Wall.java
+++ Wall.java
@@ -1,5 +1,22 @@
 package shokoban;
 
-public class Wall extends Coloumn {
+public class Wall extends  TileElement {
 
+    public void Accept (Object o, Direction d){
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Wall].Accept(object, dir):");
+
+        if this.getObject() != null)
+        o.push(o, d);
+
+        o.visit(this, d);
+        this.Remove(o);
+
+        //visszatérünk a fgv-ből
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Wall].Accept(object, dir):");
+    }
 }
