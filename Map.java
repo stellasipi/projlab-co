36fde59038a788e8db60fc9c8f279b18b99b2e8e
Tóth-Baranyi Stella <stellasipi@gmail.com>
2018-03-18 20:35:09 +0100

    szintén kiiratós kódok

--- Map.java
+++ Map.java
@@ -3,15 +3,83 @@ package shokoban;
 public class Map {
 	private TileElement[] tiles;
 	
-	public void AddTileElement(TileElement t){}
-	public void PlaceWorker(Worker w) {}
-	public void PlaceCrate(Crate c) {}
-	public void CreateMap() {}
+	public void AddTileElement(TileElement t){
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Map].AddTileElement(t):");
+
+        //fgv törzs
+
+        //visszatérünk a fgv-ből
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Map].AddTileElement(t):");
+	}
+	public void PlaceWorker(Worker w) {
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Map].PlaceWorker(t):");
+
+        //fgv törzs
+
+        //visszatérünk a fgv-ből
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Map].PlaceWorker(t):");
+	}
+	public void PlaceCrate(Crate c) {
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Map].PlaceCrate(c):");
+
+        //fgv törzs
+
+        //visszatérünk a fgv-ből
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Map].PlaceCrate(c):");
+	}
+	public void CreateMap() {
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Map].CreateMap():");
+
+        //fgv törzs
+
+        //visszatérünk a fgv-ből
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Map].CreateMap():");
+	}
 	
 	public TileElement[] getTiles() {
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Map].getTiles():");
+
+        //visszatérünk a fgv-ből
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Map].getTiles():");
+        
 		return tiles;
 	}
 	public void setTiles(TileElement[] tiles) {
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Map].setTiles(tiles):");
+        
 		this.tiles = tiles;
+		
+        //visszatérünk a fgv-ből
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Map].setTiles(tiles):");
 	}
 }
--- Target.java
+++ Target.java
@@ -9,6 +9,11 @@ public class Target extends TileElement {
 		o.push(o, d);
 		o.visit(this, d);
 		
+		//visszatérünk a fgv-ből
+		System.out.println("<");
+		System.out.println("	");
+		System.out.println("[:Target].Accept(o,d):");
+		
 	}
 	
 	public void Accept(Crate c, Direction d) {
@@ -16,16 +21,28 @@ public class Target extends TileElement {
 		System.out.println(">");
 		System.out.println("	");
 		System.out.println("[:Target].Accept(c,d):");
+		
 		c.push(c, d);
 		c.visit(this, d);
-		this.setObject(c); //ezt még meg kell írni a TileElements-be	
+		this.setObject(c); //ezt még meg kell írni a TileElements-be
+		
+		//visszatérünk a fgv-ből
+		System.out.println("<");
+		System.out.println("	");
+		System.out.println("[:Target].Accept(c,d):");
 	}
 	
 	public void Remove(Crate c) {
 		System.out.println(">");
 		System.out.println("	");
 		System.out.println("[:Target].Remove(c,d):");
+		
 		this.setObject(null);
 		c.getGame().CalculateScores(); //ez nem működik még, csak jelzés
+		
+		//visszatérünk a fgv-ből
+		System.out.println("<");
+		System.out.println("	");
+		System.out.println("[:Target].Remove(c,d):");
 	}
 }
