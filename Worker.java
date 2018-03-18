23f7636b4f21beaf90c85b399bc8627336d2c849
Tóth-Baranyi Stella <stellasipi@gmail.com>
2018-03-18 21:06:49 +0100

    Worker: teljesen jó, már csak a push(object,d) kell

--- Worker.java
+++ Worker.java
@@ -1,77 +1,183 @@
 ﻿package shokoban;
 
-
 public class Worker extends Object {
 	private int sumscore;
 	
 	public void visit(Button b, Direction d) {
-	//Ellenőrzi, hogy sikerült-e elmozdulni a másik objectnek, ha null akkor mozoghatnak, amúgy marad minden ugyanaz
-		if(b.getObject()== null) {
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Worker].visit(b, d):");
+        
+        //Ellenőrzi, hogy sikerült-e elmozdulni a másik objectnek, ha null akkor mozoghatnak, amúgy marad minden ugyanaz
+		if(b.getObject() == null) {
 			this.getTile().Remove(this); //eltávolítja magát a másikról
 			b.setObject(this);	//beállítja magát az új mezőnek
 			this.setTile(b);	//beállítj az új mezőt magának
 		}	
+		
+        //visszatérünk
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Worker].visit(b, d):");
 	}
 	
 	//Nem történik semmi, így üres
 	public void visit(Coloumn c, Direction d) {
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Worker].visit(c, d):");
+        
+        //visszatérünk
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Worker].visit(c, d):");
 	}
 	
 	public void visit(Hole h, Direction d) {
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Worker].visit(h, d):");
+		
 		//Eltávolítja magát arról a mezőről, mert meg fog halni, így tud oda lépni a következő
 		this.getTile().Remove(this);
 		getGame().Die(this);
+		
+        //visszatérünk
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Worker].visit(h, d):");
 	}
 	
 	public void visit(Target t, Direction d) {
-	//Ellenőrzi, hogy sikerült-e elmozdulni a másik objectnek, ha null akkor mozoghatnak, amúgy marad minden ugyanaz
-		if(t.getObject()== null) {
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Worker].visit(t, d):");
+		
+        //Ellenőrzi, hogy sikerült-e elmozdulni a másik objectnek, ha null akkor mozoghatnak, amúgy marad minden ugyanaz
+		if(t.getObject() == null) {
 			this.getTile().Remove(this); //eltávolítja magát a másikról
 			t.setObject(this);	//beállítja magát az új mezőnek
 			this.setTile(t);	//beállítj az új mezőt magának
 		}
+		
+        //visszatérünk
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Worker].visit(t, d):");
 	}
 	
 	public void visit(Tile t, Direction d) {
-	//Ellenőrzi, hogy sikerült-e elmozdulni a másik objectnek, ha null akkor mozoghatnak, amúgy marad minden ugyanaz
-		if(t.getObject()== null) {
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Worker].visit(t, d):");
+		
+		//Ellenőrzi, hogy sikerült-e elmozdulni a másik objectnek, ha null akkor mozoghatnak, amúgy marad minden ugyanaz
+		if(t.getObject() == null) {
 			this.getTile().Remove(this); //eltávolítja magát a másikról
 			t.setObject(this);	//beállítja magát az új mezőnek
 			this.setTile(t);	//beállítj az új mezőt magának
 		}
+		
+        //visszatérünk
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Worker].visit(t, d):");		
 	}
 
 	public void visit(Trap t, Direction d) {
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Worker].visit(t, d):");
+		
 		//ha aktív lyukként működik
 		if(t.getActive()== true) {
 			this.getTile().Remove(this);
 			getGame().Die(this);
 		}
 		//ha sima mezőként működik ellenőrzi, hogy sikerült-e elmozdulni a másik objectnek, ha null akkor mozoghatnak, amúgy marad minden ugyanaz
-		else if(t.getObject()== null){
+		else if(t.getObject() == null){
 			this.getTile().Remove(this); //eltávolítja magát a másikról
 			t.setObject(this);	//beállítja magát az új mezőnek
 			this.setTile(t);	//beállítj az új mezőt magának
 		}	
+		
+        //visszatérünk
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Worker].visit(t, d):");
 	}
 	
 	//Nem történik semmi, így üres
 	public void visit(Wall w, Direction d) {
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Worker].visit(w, d):");
+        
+        //visszatérünk
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Worker].visit(w, d):");
+		
 	}
 	
 	public void push(Object o, Direction d) {
-		
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Worker].push(o, d):");
+        
+        //visszatérünk
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Worker].push(o, d):");
 	}
 	
 	//Nem történik semmi, így üres
 	public void push(Worker w, Direction d) {
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Worker].push(w, d):");
+        
+        //visszatérünk
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Worker].push(w, d):");
 	}
 	
 	public int getSumscore() {
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Worker].getSumscore():");
+        
+        //visszatérünk
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Worker].getSumscore():");
+        
 		return sumscore;
 	}
 	public void setSumscore(int s) {
+        //bemegyünk a fgv-be
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Worker].setSumscore(s):");
+        
 		sumscore = s;
+		
+        //visszatérünk
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Worker].setSumscore(s):");
+
 	}
 
 }
