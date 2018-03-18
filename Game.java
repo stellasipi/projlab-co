2ec71cf6234657354133bf8cb0b85ba035e93681
Unknown <dianajaborszki@gmail.com>
2018-03-18 22:09:16 +0100

    Javított kiiratás, a tabok száma még nem jó

--- Game.java
+++ Game.java
@@ -4,30 +4,30 @@ import java.util.ArrayList;
 
 public class Game {
 	//Dia
-	Map map;
+	Map map=new Map();
 	ArrayList<Crate> crates= new ArrayList<>();
 	ArrayList<Worker> workers= new ArrayList<>();
 	
 	public void EndGame()
 	{
         //bemegyünk a fgv-be
-        System.out.println(">");
-        System.out.println("  ");
+        System.out.print(">");
+        System.out.print("  ");
         System.out.println("[:Game].EndGame():");
         
         //fgv törzs
         
         //bemegyünk a fgv-be
-        System.out.println("<");
-        System.out.println("  ");
+        System.out.print("<");
+        System.out.print("  ");
         System.out.println("[:Game].EndGame():");
 	}
 	
 	public void StartGame()
 	{
         //bemegyünk a fgv-be
-        System.out.println(">");
-        System.out.println("  ");
+        System.out.print(">");
+        System.out.print("  ");
         System.out.println("[:Game].StartGame():");
 
         //fgv törzs
@@ -39,23 +39,23 @@ public class Game {
         map.PlaceWorker(w);
         AddWorker(w);
         //bemegyünk a fgv-be
-        System.out.println("<");
-        System.out.println("  ");
+        System.out.print("<");
+        System.out.print("  ");
         System.out.println("[:Game].StartGame():");
 	}
 	
 	public void Move(Direction d)
 	{
         //bemegyünk a fgv-be
-        System.out.println(">");
-        System.out.println("  ");
+        System.out.print(">");
+        System.out.print("  ");
         System.out.println("[:Game].Move(d):");
 
         //fgv törzs
         
         //bemegyünk a fgv-be
-        System.out.println("<");
-        System.out.println("  ");
+        System.out.print("<");
+        System.out.print("  ");
         System.out.println("[:Game].Move(d):");
 
 	}
@@ -63,8 +63,8 @@ public class Game {
 	public void Check()
 	{
         //bemegyünk a fgv-be
-        System.out.println(">");
-        System.out.println("  ");
+        System.out.print(">");
+        System.out.print("  ");
         System.out.println("[:Game].Check():");
 
 		CheckCrateOnTarget();
@@ -73,8 +73,8 @@ public class Game {
 		CheckPushable();
         
         //bemegyünk a fgv-be
-        System.out.println("<");
-        System.out.println("  ");
+        System.out.print("<");
+        System.out.print("  ");
         System.out.println("[:Game].Check():");
 
 	}
@@ -82,43 +82,43 @@ public class Game {
 	public void AddWorker(Worker w)
 	{
         //bemegyünk a fgv-be
-        System.out.println(">");
-        System.out.println("  ");
+        System.out.print(">");
+        System.out.print("  ");
         System.out.println("[:Game].AddWorker(w):");
 
         //fgv törzs
         
         //bemegyünk a fgv-be
-        System.out.println("<");
-        System.out.println("  ");
+        System.out.print("<");
+        System.out.print("  ");
         System.out.println("[:Game].AddWorker(w):");
 	}
 	
 	public void AddCrate(Crate c)
 	{
         //bemegyünk a fgv-be
-        System.out.println(">");
-        System.out.println("  ");
+        System.out.print(">");
+        System.out.print("  ");
         System.out.println("[:Game].AddCrate(c):");
 
         //fgv törzs
         
         //bemegyünk a fgv-be
-        System.out.println("<");
-        System.out.println("  ");
+        System.out.print("<");
+        System.out.print("  ");
         System.out.println("[:Game].AddCrate(c):");
 	}
 	
 	public boolean CheckCrateOnTarget()
 	{
         //bemegyünk a fgv-be
-        System.out.println(">");
-        System.out.println("  ");
+        System.out.print(">");
+        System.out.print("  ");
         System.out.println("[:Game].CheckCrateOnTarget():");
         
         //bemegyünk a fgv-be
-        System.out.println("<");
-        System.out.println("  ");
+        System.out.print("<");
+        System.out.print("  ");
         System.out.println("[:Game].CheckCrateOnTarget():");
 		return false;
 	}
@@ -126,13 +126,13 @@ public class Game {
 	public boolean CheckDeadWorker()
 	{
         //bemegyünk a fgv-be
-        System.out.println(">");
-        System.out.println("  ");
+        System.out.print(">");
+        System.out.print("  ");
         System.out.println("[:Game].CheckDeadWorker():");
         
         //bemegyünk a fgv-be
-        System.out.println("<");
-        System.out.println("  ");
+        System.out.print("<");
+        System.out.print("  ");
         System.out.println("[:Game].CheckDeadWorker():");
 		return false;
 	}
@@ -140,13 +140,13 @@ public class Game {
 	public boolean CheckMoveableCrate()
 	{
         //bemegyünk a fgv-be
-        System.out.println(">");
-        System.out.println("  ");
+        System.out.print(">");
+        System.out.print("  ");
         System.out.println("[:Game].CheckMoveableCrate():");
         
         //bemegyünk a fgv-be
-        System.out.println("<");
-        System.out.println("  ");
+        System.out.print("<");
+        System.out.print("  ");
         System.out.println("[:Game].CheckMoveableCrate():");
 		return false;
 	}
@@ -154,13 +154,13 @@ public class Game {
 	public boolean CheckPushable()
 	{
         //bemegyünk a fgv-be
-        System.out.println(">");
-        System.out.println("  ");
+        System.out.print(">");
+        System.out.print("  ");
         System.out.println("[:Game].CheckPushable():");
         
         //bemegyünk a fgv-be
-        System.out.println("<");
-        System.out.println("  ");
+        System.out.print("<");
+        System.out.print("  ");
         System.out.println("[:Game].CheckPushable():");
 		return false;
 	}
@@ -168,43 +168,43 @@ public class Game {
 	public void CalculateScores()
 	{
         //bemegyünk a fgv-be
-        System.out.println(">");
-        System.out.println("  ");
+        System.out.print(">");
+        System.out.print("  ");
         System.out.println("[:Game].CalculateScores():");
         
         //bemegyünk a fgv-be
-        System.out.println("<");
-        System.out.println("  ");
+        System.out.print("<");
+        System.out.print("  ");
         System.out.println("[:Game].CalculateScores():");
 	}
 	
 	public void Die(Worker w)
 	{
         //bemegyünk a fgv-be
-        System.out.println(">");
-        System.out.println("  ");
+        System.out.print(">");
+        System.out.print("  ");
         System.out.println("[:Game].Die(w):");
         
         //fgv törzs
         
         //bemegyünk a fgv-be
-        System.out.println("<");
-        System.out.println("  ");
+        System.out.print("<");
+        System.out.print("  ");
         System.out.println("[:Game].Die(w):");
 	}
 	
 	public void Die(Crate c)
 	{
 	    //bemegyünk a fgv-be
-        System.out.println(">");
-        System.out.println("  ");
+        System.out.print(">");
+        System.out.print("  ");
         System.out.println("[:Game].Die(c):");
         
         //fgv törzs
         
         //bemegyünk a fgv-be
-        System.out.println("<");
-        System.out.println("  ");
+        System.out.print("<");
+        System.out.print("  ");
         System.out.println("[:Game].Die(c):");
 	}
 	
--- Map.java
+++ Map.java
@@ -5,10 +5,61 @@ import java.util.ArrayList;
 public class Map {
 	ArrayList<TileElement> tiles=new ArrayList<>();
 	
-	public void AddTileElement(TileElement t){}
-	public void PlaceWorker(Worker w) {}
-	public void PlaceCrate(Crate c) {}
-	public void CreateMap() {}
+	public void AddTileElement(TileElement t)
+	{
+		System.out.print(">");
+        System.out.print("  ");
+        System.out.println("[:Map].AddTileElement(t):");
+
+        //fgv törzs
+        
+        //bemegyünk a fgv-be
+        System.out.print("<");
+        System.out.print("  ");
+        System.out.println("[:Map].AddTileElement(t):");
+	}
+	public void PlaceWorker(Worker w) 
+	{
+		  //bemegyünk a fgv-be
+        System.out.print(">");
+        System.out.print("  ");
+        System.out.println("[:Map].PlaceWorker(w):");
+
+        //fgv törzs
+        
+        //bemegyünk a fgv-be
+        System.out.print("<");
+        System.out.print("  ");
+        System.out.println("[:Map].PlaceWorker(c):");
+	}
+	public void PlaceCrate(Crate c) 
+	{
+		  //bemegyünk a fgv-be
+        System.out.print(">");
+        System.out.print("  ");
+        System.out.println("[:Map].PlaceCrate(c):");
+
+        //fgv törzs
+        
+        //bemegyünk a fgv-be
+        System.out.print("<");
+        System.out.print("  ");
+        System.out.println("[:Map].PlaceCrate(c):");
+	}
+	public void CreateMap() 
+	{
+		  //bemegyünk a fgv-be
+        System.out.print(">");
+        System.out.print("  ");
+        System.out.println("[:Map].CreateMap():");
+
+        //fgv törzs
+        
+        //bemegyünk a fgv-be
+        System.out.print("<");
+        System.out.print("  ");
+        System.out.println("[:Map].CreateMap():");
+	}
 	
 	public ArrayList<TileElement> getTiles() {
 		return tiles;
@@ -17,3 +68,4 @@ public class Map {
 		this.tiles = tiles;
 	}
 }
+
