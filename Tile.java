ea0d5dc83c30ddd6b5e65eeb676fc9313206ba22
Unknown <dianajaborszki@gmail.com>
2018-03-18 20:12:53 +0100

    Tile

--- Tile.java
+++ Tile.java
@@ -1,7 +1,40 @@
 package shokoban;
 
 public class Tile extends TileElement {
+	public void Remove(Object o) {
+      
+        System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Tile].Remove(o):");
+
+        //függvény törzs
+        setObject(null);
+
+        //visszatérünk a függvénybõl
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Tile].Remove(o):");
+    }
+
+	@Override
 	public void Accept(Object o, Direction d) {
 		
+		System.out.println(">");
+        System.out.println("  ");
+        System.out.println("[:Tile].Accept(o,d):");
+
+        //függvény törzs
+        if(!this.getObject().equals(null))
+			o.push(o, d);
+		
+		o.visit(this, d);
+
+        //visszatérünk a függvénybõl
+        System.out.println("<");
+        System.out.println("  ");
+        System.out.println("[:Tile].Accept(o,d):");
+		
 	}
+
 }
+
