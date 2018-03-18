5e4105a766910419f1034330454d4ed1e9f4583f
Unknown <dianajaborszki@gmail.com>
2018-03-18 22:28:17 +0100

    println javítás

--- Button.java
+++ Button.java
@@ -1,46 +1,47 @@
-package shokoban;
+﻿package shokoban;
+
 
 public class Button extends TileElement {
 	private Trap trap;
 	
 	public void Accept(Object o, Direction d) {
 		//bemegyünk a fgv-be
-		System.out.println(">");
-		System.out.println("	");//itt úgy kéne, hogy annyi tab, ahányaid fgv hívás az adott dialógusba és a visszatérésnél is, csak nem tudom, hogy lenne az...
+		System.out.print(">");
+		System.out.print("	");//itt úgy kéne, hogy annyi tab, ahányaid fgv hívás az adott dialógusba és a visszatérésnél is, csak nem tudom, hogy lenne az...
 		System.out.println("[:Button].Accept(o,d):");
 		
-		if(this.getObject() != null)//amíg van a következő mezőn láda vagy munkás, addig hívjuk a push-t
+		if(!this.getObject().equals(null))//amíg van a következő mezőn láda vagy munkás, addig hívjuk a push-t
 			o.push(o, d);
 		
 		o.visit(this, d);
 		
 		//visszatérünk a fgv-ből
-		System.out.println("<");
-		System.out.println("	");
+		System.out.print("<");
+		System.out.print("	");
 		System.out.println("[:Button].Accept(o,d):");		
 	}
 	
 	public void Accept(Crate c, Direction d) {
 		//bemegyünk a fgv-be
-		System.out.println(">");
-		System.out.println("	");//itt úgy kéne, hogy annyi tab, ahányaid fgv hívás az adott dialógusba és a visszatérésnél is, csak nem tudom, hogy lenne az...
+		System.out.print(">");
+		System.out.print("	");//itt úgy kéne, hogy annyi tab, ahányaid fgv hívás az adott dialógusba és a visszatérésnél is, csak nem tudom, hogy lenne az...
 		System.out.println("[:Button].Accept(c,d):");
 		
-		if(this.getObject() != null)//amíg van a következő mezőn láda vagy munkás, addig hívjuk a push-t
+		if(!this.getObject().equals(null))//amíg van a következő mezőn láda vagy munkás, addig hívjuk a push-t
 			c.push(c, d);
 		
 		c.visit(this, d);
 		
 		//visszatérünk a fgv-ből
-		System.out.println("<");
-		System.out.println("	");
+		System.out.print("<");
+		System.out.print("	");
 		System.out.println("[:Button].Accept(c,d):");		
 	}
 	
 	public void Remove(Crate c) {
 		//bemegyünk a fgv-be
-		System.out.println(">");
-		System.out.println("	");
+		System.out.print(">");
+		System.out.print("	");
 		System.out.println("[:Button].Remove(c):");
 		
 		//fgv törzs
@@ -48,15 +49,15 @@ public class Button extends TileElement {
 		c.setTile(null); //ebbe azért nem vagyok biztos
 		
 		//visszatérünk a fgv-ből
-		System.out.println("<");
-		System.out.println("	");
+		System.out.print("<");
+		System.out.print("	");
 		System.out.println("[:Button].Remove(c):");
 	}
 	
 	public void Switch(Trap t) {
 		//bemegyünk a fgv-be
-		System.out.println(">");
-		System.out.println("	");
+		System.out.print(">");
+		System.out.print("	");
 		System.out.println("[:Button].Switch(t):");
 		
 		if(t.getActive())
@@ -65,20 +66,20 @@ public class Button extends TileElement {
 			t.setActive(false); //inaktívra állítjuk a csapdát
 		
 		//visszatérünk a fgv-ből
-		System.out.println("<");
-		System.out.println("	");
+		System.out.print("<");
+		System.out.print("	");
 		System.out.println("[:Button].Switch(c):");
 	}
 
 	public Trap getTrap() {
 		//bemegyünk a fgv-be
-		System.out.println(">");
-		System.out.println("	");
+		System.out.print(">");
+		System.out.print("	");
 		System.out.println("[:Button].getTrap():");
 		
 		//visszatérünk a fgv-ből
-		System.out.println("<");
-		System.out.println("	");
+		System.out.print("<");
+		System.out.print("	");
 		System.out.println("[:Button].getTrap():");
 		
 		return trap;
@@ -86,15 +87,15 @@ public class Button extends TileElement {
 
 	public void setTrap(Trap trap) {
 		//bemegyünk a fgv-be
-		System.out.println(">");
-		System.out.println("	");
+		System.out.print(">");
+		System.out.print("	");
 		System.out.println("[:Button].setTrap(trap):");
 		
 		this.trap = trap;
 		
 		//visszatérünk a fgv-ből
-		System.out.println("<");
-		System.out.println("	");
+		System.out.print("<");
+		System.out.print("	");
 		System.out.println("[:Button].setTrap(trap):");		
 	}
 	
--- Coloumn.java
+++ Coloumn.java
@@ -1,20 +1,21 @@
-package shokoban;
+﻿package shokoban;
 
 public class Coloumn extends TileElement {
     public void Accept (Object o, Direction d){
         //bemegyünk a fgv-be
-        System.out.println(">");
-        System.out.println("  ");
-        System.out.println("[:Coloumn].Accept(o, d):");
+        System.out.print(">");
+        System.out.print("  ");
+        System.out.println("[:Coloumn].Accept(object, dir):");
 
-        if (!this.getObject().equals(null))
+        if (this.getObject() != null)
         o.push(o, d);
 
         o.visit(this, d);
+        this.Remove(o);
 
         //visszatérünk a fgv-ből
-        System.out.println("<");
-        System.out.println("  ");
-        System.out.println("[:Coloumn].Accept(o, d):");
+        System.out.print("<");
+        System.out.print("  ");
+        System.out.println("[:Coloumn].Accept(object, dir):");
     }
 }
