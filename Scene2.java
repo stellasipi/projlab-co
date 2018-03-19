c0089c410570a014bd9acd25ce7eec14a7fedfa6
Livi <olahlivi@gmail.com>
2018-03-19 15:15:06 +0100

    tile:accept

--- Scene2.java
+++ Scene2.java
@@ -1,19 +1,16 @@
 package shokoban;
 
 public class Scene2 {
-	
-	public void Scene2_1() {
-		Game game = new Game();
-		Worker worker = new Worker();
-		Crate crate = new Crate();
-		Map m = new Map();
-		Tile t1 = new Tile();
-		Tile t2 = new Tile();
-		Wall w = new Wall();
+	Game game = new Game();
+	Worker worker = new Worker();
+	Crate crate = new Crate();
+	Map m = new Map();
+	Tile t1 = new Tile();
+	Tile t2 = new Tile();
+
+	public void Scene2set() {
 		t1.setNeighbour(t2, Direction.RIGHT);
 		t2.setNeighbour(t1, Direction.LEFT);
-		t2.setNeighbour(w, Direction.RIGHT);
-		w.setNeighbour(t2, Direction.LEFT);
 		t1.setObject(worker);
 		t2.setObject(crate);
 		worker.setTile(t1);
@@ -22,6 +19,14 @@ public class Scene2 {
 		game.AddWorker(worker);
 		m.AddTileElement(t1);
 		m.AddTileElement(t2);
+	}
+	
+	public void Scene2_1() {
+		
+		this.Scene2set();
+		Wall w = new Wall();
+		t2.setNeighbour(w, Direction.RIGHT);
+		w.setNeighbour(t2, Direction.LEFT);
 		m.AddTileElement(w);
 		
 		t2.Accept(worker, Direction.RIGHT);
