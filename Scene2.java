5ab35402ed5ad4e3ec74b3e6c1f0a6c6888a5c0c
Livi <olahlivi@gmail.com>
2018-03-19 13:59:50 +0100

    scene2

--- Scene2.java
+++ Scene2.java
@@ -2,4 +2,13 @@ package shokoban;
 
 public class Scene2 {
 	
+	public void Scene2_1() {
+		
+	}
+	public void Scene2_2() {}
+	public void Scene2_3() {}
+	public void Scene2_4() {}
+	public void Scene2_5() {}
+	public void Scene2_6() {}
+	public void Scene2_7() {}
 }
