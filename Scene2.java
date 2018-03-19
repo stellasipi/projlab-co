2b06aab3ea2747b09fe6b72de3562ee99b29e53d
Livi <olahlivi@gmail.com>
2018-03-19 14:11:57 +0100

    scene 2

--- Scene2.java
+++ Scene2.java
@@ -3,6 +3,13 @@ package shokoban;
 public class Scene2 {
 	
 	public void Scene2_1() {
+		Game game = new Game();
+		Map m = new Map();
+		Tile t = new Tile();
+		Wall w = new Wall();
+		
+		m.AddTileElement(t);
+		m.AddTileElement(t);
 		
 	}
 	public void Scene2_2() {}
