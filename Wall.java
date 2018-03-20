18563c278f2d1bbcdc9b6e86f4621d6eac644150
Marci <radly96@gmail.com>
2018-03-20 20:39:16 +0100

    wall, tileElement

--- Wall.java
+++ Wall.java
@@ -1,21 +1,43 @@
 ﻿package shokoban;
 
 public class Wall extends Coloumn {
+	
 	@Override
-    public void Accept (Object o, Direction d){
+    public void Accept (Worker w, Direction d){
         //bemegyünk a fgv-be
         System.out.print(">");
         System.out.print("  ");
-        System.out.println("[:Wall].Accept(object, d):");
+        System.out.println("[:Wall].Accept(worker, d):");
         
       //Meghívja object push függvényét, hogy el tudja tolni a rajta levő elemet
         if(this.getObject() != null)  
         {	
-			o.getTile().getNeighbour(d).getObject().push(o, d);
+			w.getTile().getNeighbour(d).getObject().push(w, d);
         }
 
-        o.visit(this, d);
-        this.Remove(o);
+        w.visit(this, d);
+        this.Remove(w);
+    }
+	
+	@Override
+    public void Remove(Worker w) {
+    }
+	
+	@Override
+    public void Accept (Crate c, Direction d){
+        //bemegyünk a fgv-be
+        System.out.print(">");
+        System.out.print("  ");
+        System.out.println("[:Wall].Accept(crate, d):");
+        
+      //Meghívja object push függvényét, hogy el tudja tolni a rajta levő elemet
+        if(this.getObject() != null)  
+        {	
+			c.getTile().getNeighbour(d).getObject().push(c, d);
+        }
+
+        c.visit(this, d);
+        this.Remove(c);
     }
 	
 	@Override
