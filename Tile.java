4e5408df6aadf65b2ef56189a9211825cc4e2e68
Livi <olahlivi@gmail.com>
2018-03-20 20:42:15 +0100

    tile-accept-remove

--- Tile.java
+++ Tile.java
@@ -1,29 +1,52 @@
 ﻿package shokoban;
 
 public class Tile extends TileElement {
-	public void Remove(Object o) {
+	public void Remove(Worker w) {
       
         System.out.print(">");
         System.out.print("  ");
-        System.out.println("[:Tile].Remove(o):");
+        System.out.println("[:Tile].Remove(w):");
+
+        //függvénytörzs
+        setObject(null);
+    }
+	
+	public void Remove(Crate c) {
+	      
+        System.out.print(">");
+        System.out.print("  ");
+        System.out.println("[:Tile].Remove(c):");
 
         //függvénytörzs
         setObject(null);
     }
 
-	@Override
-	public void Accept(Object o, Direction d) {
+	public void Accept(Worker w, Direction d) {
 		System.out.print(">");
         System.out.print("  ");
-        System.out.println("[:Tile].Accept(o,d):");
+        System.out.println("[:Tile].Accept(w,d):");
 
       //függvénytörzs
         if(this.getObject() != null) 
         {
-        	o.getTile().getNeighbour(d).getObject().push(o, d); //Meghívja object push függvényét, hogy el tudja tolni a rajta levő elemet
+        	w.getTile().getNeighbour(d).getObject().push(w, d); //Meghívja object push függvényét, hogy el tudja tolni a rajta levő elemet
 
         }
-		o.visit(this, d); //Meghívja az object visit függvényét, hogy ráléphessen a mezőre	
+		w.visit(this, d); //Meghívja az object visit függvényét, hogy ráléphessen a mezőre	
+	}
+	
+	public void Accept(Crate c, Direction d) {
+		System.out.print(">");
+        System.out.print("  ");
+        System.out.println("[:Tile].Accept(c,d):");
+
+      //függvénytörzs
+        if(this.getObject() != null) 
+        {
+        	c.getTile().getNeighbour(d).getObject().push(c, d); //Meghívja object push függvényét, hogy el tudja tolni a rajta levő elemet
+
+        }
+		c.visit(this, d); //Meghívja az object visit függvényét, hogy ráléphessen a mezőre	
 	}
 
 }
