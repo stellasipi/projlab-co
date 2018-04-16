ba540e08a2380e5bffc930fe6c4b95e31b8e386f
Livi <olahlivi@gmail.com>
2018-04-16 23:12:55 +0200

    Tile

--- Tile.java
+++ Tile.java
@@ -1,6 +1,10 @@
 package shokoban;
 
+import java.util.ArrayList;
+
 public class Tile extends TileElement {
+	private ArrayList<Grease> grease = new ArrayList<Grease>();
+	
 	public void Remove(Worker w) {
       
         System.out.print(">");
@@ -29,7 +33,7 @@ public class Tile extends TileElement {
       //függvénytörzs
         if(this.getObject() != null) 
         {
-        	w.getTile().getNeighbour(d).getObject().push(w, d); //Meghívja object push függvényét, hogy el tudja tolni a rajta levő elemet
+        	w.getTile().getNeighbour(d).getObject().push(w, d, acts); //Meghívja object push függvényét, hogy el tudja tolni a rajta levő elemet
 
         }
 		w.visit(this, d); //Meghívja az object visit függvényét, hogy ráléphessen a mezőre	
@@ -43,10 +47,22 @@ public class Tile extends TileElement {
       //függvénytörzs
         if(this.getObject() != null) 
         {
-        	c.getTile().getNeighbour(d).getObject().push(c, d); //Meghívja object push függvényét, hogy el tudja tolni a rajta levő elemet
+        	c.getTile().getNeighbour(d).getObject().push(c, d, acts); //Meghívja object push függvényét, hogy el tudja tolni a rajta levő elemet
 
         }
 		c.visit(this, d); //Meghívja az object visit függvényét, hogy ráléphessen a mezőre	
+	}
+	
+	public int SumGreaseMu() {
+		int sum=0;
+		for(int i =0; i<grease.size(); i++) {
+			sum=sum+grease.get(i).getMu();
+		}
+		return sum;
+	}
+	
+	public void setGrease(Grease g) {
+		grease.add(g);
 	}
 
 }
