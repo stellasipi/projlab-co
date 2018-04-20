b1e55aef068516b7b29c0663e326aafbb002802a
Livi <olahlivi@gmail.com>
2018-04-20 13:15:02 +0200

    közös jav

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
 package shokoban;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 
 		System.out.println("Menu:\n");
+		//Itt hívja meg Dávid a tesztelő részt
 	}
-    	
 }
 	
 		
