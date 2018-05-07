1bcc46fa8db352c19b7d0e3e04e6f26baad22f68
Livi <olahlivi@gmail.com>
2018-05-07 21:35:24 +0200

    draws

--- DrawnColoumn.java
+++ DrawnColoumn.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 
 public class DrawnColoumn extends Drawable {
 	private Coloumn c;
@@ -16,5 +17,11 @@ public class DrawnColoumn extends Drawable {
 		setImg_label(new JLabel(new ImageIcon(getImg()))); // így már egy komponens és majd hozzá tudjuk adni a mapPanel-hez
 		
 		setDepth(new Integer(1));//mélység beállítása
+	}
+	
+	public void Draw(GameView gw) {
+		JLayeredPane a = gw.getTiles(c.getCoords()[0], c.getCoords()[1]);
+		a.add(getImg_label(),getDepth());
+		gw.setTiles(c.getCoords()[0], c.getCoords()[1], a);	
 	}
 }
--- DrawnCrate.java
+++ DrawnCrate.java
@@ -6,26 +6,22 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 
 public class DrawnCrate extends Drawable {
 	private Crate c;
 	
 	public DrawnCrate(Crate cr) throws IOException {
 		c=cr;
-		Integer[] coords = c.getTile().getCoords();
-		this.setX(coords[0]);
-		this.setY(coords[1]);
 		setImg(ImageIO.read(new File("pics/Crate.png"))); // kép beolvasása
 		setImg_label(new JLabel(new ImageIcon(getImg()))); // így már egy komponens és majd hozzá tudjuk adni a mapPanel-hez
 		
 		setDepth(new Integer(3));//mélység beállítása
 	}
 
-	@Override
-	public void Draw(GameView gv){
-		Integer[] coords = c.getTile().getCoords();
-		this.setX(coords[0]);
-		this.setY(coords[1]);
-		gv.setTiles(getX(), getY(), this);
+	public void Draw(GameView gw) {
+		JLayeredPane a = gw.getTiles(c.getTile().getCoords()[0], c.getTile().getCoords()[1]);
+		a.add(getImg_label(),getDepth());
+		gw.setTiles(c.getTile().getCoords()[0], c.getTile().getCoords()[1], a);	
 	}
 }
