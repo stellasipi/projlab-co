e8da0d44184b6fa100da935a40c6f76662f16500
Unknown <stellasipi@gmail.com>
2018-05-07 21:38:06 +0200

    hole

--- DrawnHole.java
+++ DrawnHole.java
@@ -6,18 +6,23 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 
 public class DrawnHole extends Drawable {
 	private Hole h;
 	
 	public DrawnHole(Hole hole) throws IOException {
 		h=hole;
-		Integer[] coords = h.getCoords();
-		this.setX(coords[0]);
-		this.setY(coords[1]);
+
 		setImg(ImageIO.read(new File("pics/Hole.png"))); // kép beolvasása
 		setImg_label(new JLabel(new ImageIcon(getImg()))); // így már egy komponens és majd hozzá tudjuk adni a mapPanel-hez
 		
 		setDepth(new Integer(1));//mélység beállítása
+	}
+	
+	public void Draw(GameView gw) {
+		JLayeredPane a = gw.getTiles(h.getCoords()[0], h.getCoords()[1]);
+		a.add(getImg_label(),getDepth());
+		gw.setTiles(h.getCoords()[0], h.getCoords()[1], a);	
 	}
 }
