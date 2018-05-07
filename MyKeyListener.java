2e728db89585894c77c5e0d6cf0e0b944ab025fa
Unknown <dianajaborszki@gmail.com>
2018-05-07 11:13:37 +0200

    switch váz

--- MyKeyListener.java
+++ MyKeyListener.java
@@ -29,8 +29,8 @@ import javax.swing.*;
 		}
 
 		public void keyPressed(KeyEvent e) {
-			/*int key = e.getKeyCode();
-			if(key==e.VK_SPACE) {
+			int key = e.getKeyCode();
+			/*if(key==e.VK_SPACE) {
 				pressed.add(e.getKeyCode());
 				System.out.println("keyPressed="+KeyEvent.getKeyText(e.getKeyCode()));
 			}
@@ -39,6 +39,81 @@ import javax.swing.*;
 				System.out.println("keyPressed="+KeyEvent.getKeyText(e.getKeyCode()));
 			}
 			else if(key==e.VK_LEFT) {*/
+			switch(key)
+			{
+			case KeyEvent.VK_W:
+				break;
+				
+			case KeyEvent.VK_A:
+				break;
+				
+			case KeyEvent.VK_S:
+				break;
+				
+			case KeyEvent.VK_D:
+				break;
+				
+			case KeyEvent.VK_Q:
+				break;	
+				
+			case KeyEvent.VK_E:
+				break;
+				
+			case KeyEvent.VK_I:
+				break;
+				
+			case KeyEvent.VK_J:
+				break;
+				
+			case KeyEvent.VK_K:
+				break;
+				
+			case KeyEvent.VK_L:
+				break;
+				
+			case KeyEvent.VK_U:
+				break;	
+				
+			case KeyEvent.VK_P:
+				break;
+				
+			case KeyEvent.VK_UP:
+				break;
+				
+			case KeyEvent.VK_LEFT:
+				break;
+				
+			case KeyEvent.VK_RIGHT:
+				break;
+				
+			case KeyEvent.VK_DOWN:
+				break;
+				
+			case KeyEvent.VK_0:
+				break;	
+				
+			case KeyEvent.VK_1:
+				break;
+				
+			case KeyEvent.VK_8:
+				break;
+				
+			case KeyEvent.VK_4:
+				break;
+				
+			case KeyEvent.VK_5:
+				break;
+				
+			case KeyEvent.VK_6:
+				break;
+				
+			case KeyEvent.VK_7:
+				break;	
+				
+			case KeyEvent.VK_9:
+				break;
+			}
+			
 				pressed.add(e.getKeyCode());
 				System.out.println("keyPressed="+KeyEvent.getKeyText(e.getKeyCode()));
 			//}
