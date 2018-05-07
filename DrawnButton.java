8826dbf1dc4a51616b7244f4101c1ca6ffa3f22e
Dávid <davemill96@gmail.com>
2018-05-07 16:27:31 +0200

    minta

--- DrawnButton.java
+++ DrawnButton.java
@@ -12,6 +12,9 @@ public class DrawnButton extends Drawable {
 	
 	public DrawnButton(Button button) throws IOException {
 		b=button;
+		Integer[] coords = b.getCoords();
+		this.setX(coords[0]);
+		this.setY(coords[1]);
 		setImg(ImageIO.read(new File("pics/Button.png"))); // kép beolvasása
 		setImg_label(new JLabel(new ImageIcon(getImg()))); // így már egy komponens és majd hozzá tudjuk adni a mapPanel-hez
 		
